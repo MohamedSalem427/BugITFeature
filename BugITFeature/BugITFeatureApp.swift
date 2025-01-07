4eaf122294ec127c70d496caea50338b0874c94b
MohamedSalem427 <53254215+MohamedSalem427@users.noreply.github.com>
2025-01-07 19:59:02 +0200

    add dependencies

--- BugITFeature/BugITFeatureApp.swift
+++ BugITFeature/BugITFeatureApp.swift
@@ -6,12 +6,38 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import GoogleSignIn
 
 @main
 struct BugITFeatureApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             BugReportScreen()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ application: UIApplication,
+                     open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        let handled = GIDSignIn.sharedInstance.handle(url)
+        if handled { return true }
+        return false
+    }
+    
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        let handled = GIDSignIn.sharedInstance.handle(url)
+        if handled { return true }
+        return false
     }
 }
