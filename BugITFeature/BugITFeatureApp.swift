fcc2b819c0ea94f469ebc048932552236f6d50d3
MohamedSalem427 <53254215+MohamedSalem427@users.noreply.github.com>
2025-01-07 10:04:19 +0200

    Initial Commit

---
+++ BugITFeature/BugITFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugITFeatureApp.swift
+//  BugITFeature
+//
+//  Created by Mohamed Salem on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct BugITFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
